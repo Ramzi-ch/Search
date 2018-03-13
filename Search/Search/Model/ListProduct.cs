aafcba570c4d7a8a3f5d38accdd54b26bd6fc1ec
Ramzi Chakroun <r.chakroun@root.sa>
2018-03-13 16:46:17 +0100

    Add project files.

---
+++ Search/Search/Model/ListProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search
+{
+    public class ListProduct
+    {
+        public String Image { get; set; }
+        public String Name { get; set; }
+        public String Type { get; set; }
+        public String Price { get; set; }
+    }
+}
---
+++ Search/Search/Views/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Search
+{
+	public partial class MainPage : ContentPage
+	{
+        List<ListProduct> ListP;
+
+        public MainPage()
+		{
+			InitializeComponent();
+            ListP = new List<ListProduct>
+            {
+                new ListProduct{Image="bag.png", Name="Gucci Hand Bag Light Brown", Type="Almost New", Price="3000.00 SAR"},
+                new ListProduct{Image="bag2.png", Name="Andre Hand Bag Light Brown", Type="Almost New", Price="3000.00 SAR"},
+                new ListProduct{Image="bag3.png", Name="Prada Hand Bag Light Brown", Type="Almost New", Price="3000.00 SAR"}
+            };
+            List1.ItemsSource = ListP;
+
+        }
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //thats all you need to make a search  
+
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                List1.ItemsSource = ListP;
+            }
+
+            else
+            {
+                List1.ItemsSource = ListP.Where(x => x.Name.ToLower().StartsWith(e.NewTextValue.ToLower()));
+            }
+        }
+    }
+}
